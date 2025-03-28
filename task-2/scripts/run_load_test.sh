9ac4990ab5142a966d13ab248d4163bb43f127af
Daniel Kim <dhjkim2001@gmail.com>
2025-03-28 09:57:56 +0000

    Changeable parameters

--- task-2/scripts/run_load_test.sh
+++ task-2/scripts/run_load_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-python serving_rag.py --batch_size 8 --max_waiting_time 10 &
+
+BATCH_SIZE=8
+MAX_WAITING_TIME=10
+NUM_USERS=2
+NUM_REQUESTS=5
+
+python serving_rag.py --batch_size $BATCH_SIZE --max_waiting_time $MAX_WAITING_TIME &
 SERVER_PID=$!
 
 echo "Waiting for server to start on port 8000..."
@@ -8,8 +14,7 @@ while ! nc -z localhost 8000; do
 done
 echo "Server is up!"
 
-python -m modules.load_tester --num_requests 5 --num_users 2
+python -m modules.load_tester --num_users $NUM_USERS --num_requests $NUM_REQUESTS
 
-# Shut down the server process
 echo "Killing server with PID $SERVER_PID"
 kill -9 $SERVER_PID
